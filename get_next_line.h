b6858d95fed157162bc8b4cdd7f95c656da79b23
Stanley Horwood <stanley@horwood.fr>
2018-11-09 06:18:17 +0100

    removed dependency from limits.h

--- get_next_line.h
+++ get_next_line.h
@@ -6,16 +6,15 @@
 /*   By: shorwood <shorwood@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/10/16 05:01:38 by shorwood     #+#   ##    ##    #+#       */
-/*   Updated: 2018/11/09 01:31:03 by shorwood    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/11/09 06:18:06 by shorwood    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #ifndef _GET_NEXT_LINE_H
 # define _GET_NEXT_LINE_H
-# include <limits.h>
 # define BUFF_SIZE 1024
-# define GNL_FD_MAX OPEN_MAX
+# define GNL_FD_MAX 1024
 
 int	get_next_line(const int fd, char **line);
 #endif
