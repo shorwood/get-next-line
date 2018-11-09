59f8fd68771e390e2262584999492d909228a602
Stanley Horwood <stanley@horwood.fr>
2018-11-09 01:31:24 +0100

    buffsize set to 1kB

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: shorwood <shorwood@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/10/16 05:01:38 by shorwood     #+#   ##    ##    #+#       */
-/*   Updated: 2018/11/08 23:11:01 by shorwood    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/11/09 01:31:03 by shorwood    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -14,7 +14,7 @@
 #ifndef _GET_NEXT_LINE_H
 # define _GET_NEXT_LINE_H
 # include <limits.h>
-# define BUFF_SIZE 1
+# define BUFF_SIZE 1024
 # define GNL_FD_MAX OPEN_MAX
 
 int	get_next_line(const int fd, char **line);
